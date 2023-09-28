2d7e39b5af817aa0b68d7a135eef00a1aad482c7
RealKieDo <tu4200733@taibahu.edu.sa>
2023-09-28 11:54:14 +0300

    init

--- Test_Project01.java
+++
@@ -1,6 +0,0 @@
-public class Test_Project01 {
-    public static void main(String[] args) {
-
-        System.out.println("Hello everyone");
-    }
-}
---
+++ test_project/test_project01.java
@@ -0,0 +1,10 @@
+package test_project;
+
+ class test_project01 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hello everyone");
+    }
+}
